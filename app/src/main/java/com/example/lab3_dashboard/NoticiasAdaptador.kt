eee92cca06db7adf62a280eef99f8e5e47ad1324
GitElean <eleanjulian@gmail.com>
2021-03-11 13:56:08 -0600

    "commit prueba"

--- app/src/main/java/com/example/lab3_dashboard/NoticiasAdaptador.kt
+++ app/src/main/java/com/example/lab3_dashboard/NoticiasAdaptador.kt
@@ -8,9 +8,10 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.squareup.picasso.Picasso
 
-public class NoticiasAdaptador(private var noticias: MutableList<Noticia>) : RecyclerView.Adapter<NoticiasAdaptador.NoticiasHolder>() {
+public class NoticiasAdaptador(private val listener: MutableList<Noticia>):
+        RecyclerView.Adapter<NoticiasAdaptador.NoticiasHolder>() {
 
-
+    private var noticias: MutableList<Noticia> = mutableListOf()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoticiasHolder {
         val view: View = LayoutInflater.from(parent.context).inflate(R.layout.item_noticia, parent, false)
@@ -19,7 +20,7 @@ public class NoticiasAdaptador(private var noticias: MutableList<Noticia>) : Rec
 
     override fun onBindViewHolder(holder: NoticiasHolder, position: Int) {
         val actual: Noticia = this.noticias[position]
-        holder.bind(actual)
+        holder.bind(actual, listener)
 
     }
 
@@ -31,11 +32,27 @@ public class NoticiasAdaptador(private var noticias: MutableList<Noticia>) : Rec
      *
      */
     class NoticiasHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
-        fun bind(noticia: Noticia) = with(itemView){
+        fun bind(noticia: Noticia, listener: MutableList<Noticia>) = with(itemView){
             val txtTitle:TextView = findViewById(R.id.txtTitle)
             val imagen: ImageView = findViewById(R.id.imagen)
             txtTitle.text = noticia.titulo
             Picasso.get().load(noticia.imagen).into(imagen);
+
+            setOnClickListener{
+                listener.onItemClicked(adapterPosition)
+            }
+
+            setOnLongClickListener{
+                listener.onItemLongClicked(adapterPosition)
+            }
+        }
+
+
+
+        interface ClickListener{
+            fun onItemClicked(position: Int)
+
+            fun onItemLongClicked(position: Int): Boolean
         }
     }
 }
